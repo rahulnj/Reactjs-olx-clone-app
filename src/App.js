4588fd968ceed1bdbefe50554b87018790971c18
Rahul <rahuljayaraman8@gmail.com>
2021-11-29 10:40:26 +0530

    signup page style changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home';
+import Signup from './Pages/Signup'
 
 function App() {
   return (
     <div>
-      <Home />
-    </div>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />}>
+          </Route>
+          <Route exact path='/signup' element={<Signup />}>
+          </Route>
+        </Routes>
+      </Router>
+    </div >
   );
 }
 
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Logo from '../../olx-logo.png';
 import './Signup.css';
 
+import Logo from '../../olx-logo.png';
 export default function Signup() {
     return (
         <div>
