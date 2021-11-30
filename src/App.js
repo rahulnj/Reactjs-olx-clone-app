e4dc0cd6da4c3252489f0ff990ba73795515fcb2
Rahul <rahuljayaraman8@gmail.com>
2021-11-30 19:35:12 +0530

    image uploaded

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useContext } from 'react';
 import './App.css';
-
+import { AuthContext, FirebaseContext } from './store/Context';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
 import Home from './Pages/Home';
 import SignupPage from './Pages/Signup'
 import LoginPage from './Pages/Login';
-import { AuthContext, FirebaseContext } from './store/Context';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Create from './Components/Create/Create';
 
 function App() {
   const { user, setUser } = useContext(AuthContext)
@@ -29,6 +31,8 @@ function App() {
           <Route exact path='/signup' element={<SignupPage />}>
           </Route>
           <Route exact path='/login' element={<LoginPage />}>
+          </Route>
+          <Route exact path='/Create' element={<Create />}>
           </Route>
         </Routes>
       </Router>
--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,47 +1,90 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import './Create.css';
 import Header from '../Header/Header';
-
+import { FirebaseContext, AuthContext } from '../../store/Context';
+import {
+    getFirestore, collection, addDoc
+} from 'firebase/firestore'
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 const Create = () => {
+    const { user } = useContext(AuthContext)
+    const [name, setName] = useState("")
+    const [category, setCategory] = useState("")
+    const [price, setPrice] = useState("")
+    const [image, setImage] = useState(null)
+
+    const db = getFirestore()
+    const colRef = collection(db, 'products')
+    const storage = getStorage();
+    let date = new Date()
+    const handleSubmit = () => {
+        const ImagesRef = ref(storage, `images/${image.name}`);
+        uploadBytes(ImagesRef, image).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+
+                console.log('Uploaded a blob or file!');
+                console.log(snapshot);
+                addDoc(colRef, {
+                    name,
+                    category,
+                    price,
+                    downloadURL,
+                    userId: user.uid,
+                    createdAt: date.toDateString()
+                });
+            })
+
+
+
+        });
+
+
+    }
+
+
+
     return (
         <Fragment>
             <Header />
             <card>
                 <div className="centerDiv">
-                    <form>
-                        <label htmlFor="fname">Name</label>
-                        <br />
-                        <input
-                            className="input"
-                            type="text"
-                            id="fname"
-                            name="Name"
-                            defaultValue="John"
-                        />
-                        <br />
-                        <label htmlFor="fname">Category</label>
-                        <br />
-                        <input
-                            className="input"
-                            type="text"
-                            id="fname"
-                            name="category"
-                            defaultValue="John"
-                        />
-                        <br />
-                        <label htmlFor="fname">Price</label>
-                        <br />
-                        <input className="input" type="number" id="fname" name="Price" />
-                        <br />
-                    </form>
+
+                    <label htmlFor="fname">Name</label>
                     <br />
-                    <img alt="Posts" width="200px" height="200px" src=""></img>
-                    <form>
-                        <br />
-                        <input type="file" />
-                        <br />
-                        <button className="uploadBtn">upload and Submit</button>
-                    </form>
+                    <input onChange={(e) => setName(e.target.value)}
+                        className="input"
+                        type="text"
+                        id="fname"
+                        name="Name"
+                    />
+                    <br />
+                    <label htmlFor="fname">Category</label>
+                    <br />
+                    <input onChange={(e) => setCategory(e.target.value)}
+                        className="input"
+                        type="text"
+                        id="fname"
+                        name="category"
+                    />
+                    <br />
+                    <label htmlFor="fname">Price</label>
+                    <br />
+                    <input onChange={(e) => setPrice(e.target.value)}
+                        className="input" type="number" id="fname" name="Price" />
+                    <br />
+
+                    <br />
+                    <img alt="image" width="200px" height="200px" src={image ? URL.createObjectURL(image) : ''}></img>
+
+                    <br />
+                    <input onChange={(e) => {
+                        setImage(e.target.files[0])
+                    }}
+                        type="file" />
+                    <br />
+                    <button onClick={handleSubmit} className="uploadBtn">Submit</button>
+
                 </div>
             </card>
         </Fragment>
