ec2ca796180f447429e4d010a8748a44a32faafe
Rahul <rahuljayaraman8@gmail.com>
2021-12-29 19:43:54 +0530

    changes made in the context api

--- src/store/PostContext.js
+++ src/store/PostContext.js
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const PostContext = createContext(null)
 
-function Post({ children }) {
+const Post = ({ children }) => {
     const [postDetails, setPostDetails] = useState()
     console.log(postDetails);
     return (
